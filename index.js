ec779571ec2fef6328b183c830410ecab222ffea
Greg Dracoulis <gdp.spamcatcher@gmail.com>
2016-04-15 00:50:17 -0400

    Print out nearby WiFi SSIDs

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('Hello world');
+var WiFiControl = require('wifi-control');
+
+//  Initialize wifi-control package with verbose output
+WiFiControl.init({
+  debug: true
+});
+
+//  Try scanning for access points:
+WiFiControl.scanForWiFi(function(err, response) {
+  if (err) console.log(err);
+  console.log(response);
+});
