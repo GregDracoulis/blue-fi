28bb51b96d748cad57c8a7be97de13a49d641f78
Greg Dracoulis <gdp.spamcatcher@gmail.com>
2016-04-17 22:40:31 -0400

    Add super basic connection test example

---
+++ examples/connection-test/index.js
@@ -0,0 +1,42 @@
+var noble = require('noble');
+
+var serviceUuid = 'fffffffffffffffffffffffffffffff0';
+var characteristicUuid = 'fffffffffffffffffffffffffffffff1';
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    console.log('scanning');
+    noble.startScanning([serviceUuid], false);
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  noble.stopScanning();
+
+  console.log('found peripheral: ', peripheral.advertisement);
+  peripheral.connect(function(err) {
+    peripheral.discoverServices([serviceUuid], function(err, services) {
+      services.forEach(function(service) {
+        console.log('found service: ' + service.uuid);
+        service.discoverCharacteristics([], function(err, characteristics) {
+          characteristics.forEach(function(characteristic) {
+            console.log('found characteristic: ' + characteristic.uuid);
+            if (characteristicUuid == characteristic.uuid) {
+              console.log('found matching characteristic')
+              characteristic.read(function(data) {
+                console.log(data);
+                try {
+                  console.log(JSON.parse(data));
+                } catch (e) {
+                  console.log(e);
+                }
+              });
+            }
+          });
+        });
+      });
+    });
+  });
+});
